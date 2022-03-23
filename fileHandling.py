49591e4b1cf40f90bb761f0d31395695955205b5
Joon S. Lee <jlee@vsu.edu>
2022-03-23 18:32:25 -0400

    file handling example added

--- fileHandling.py
+++ fileHandling.py
@@ -2,12 +2,14 @@ import sys
 
 argCount = len(sys.argv)
 args = sys.argv
+filename = ""
+arraySize = -1
 
 if argCount == 2:
-    print(f"filename is {args[1]}")
+    filename = args[1]
 elif argCount == 3:
-    print(f"array size is {args[1]}")
-    print(f"filename is {args[2]}")
+    filename = args[2]
+    arraySize = args[1]
 else:
     print("This program should be invoked from the command-line as:")
     print("python3 <name of python script file> <command-file>")
@@ -15,8 +17,15 @@ else:
     print("python3 <name of python script file> <array size> <command-file>")
     exit(0)
 
-# need to have two variables
-inputfilename = ""
-arraysize = -1
+with open(filename, "r") as ifile:
+    lines = ifile.readlines()
 
+for line in lines:
+    print(line)
+
+if argCount == 3:
+    print(f"array size is: {args[1]}")
+else:
+    print("default array size is used")
+    
 
